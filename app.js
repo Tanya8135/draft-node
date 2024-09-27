806dfbcd7d52b2f7ce9b24951f75ffb32cf7119d
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-09-27 12:36:12 +0300

    refresh app.js

--- app.js
+++ app.js
@@ -4,14 +4,6 @@ const aboutRouter = require('./routes/api/about')
 const app = express();
 
 
-
-
-
-
-// app.get('/', (req, res) => {
-//     res.send('Hellow, World! http://localhost:3000/');
-// });
-
 // Middleware для парсинга формы
 app.use(express.urlencoded({ extended: false }));
 // app.use(express.static('public')); /* frontend */
@@ -19,11 +11,6 @@ app.use(express.json()); /* for POSTMAN */
 
 app.use('/', aboutRouter);
 
-// Обработка POST-запроса от формы
-// app.post('/login', (req, res) => {
-//     const { email, password } = req.body;
-//     // Обработка данных
-//     res.send(`Email: ${email}, Пароль: ${password}`);
-// });
+
 
 module.exports = app;
